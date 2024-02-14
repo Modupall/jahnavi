832564d2c590d477fbc1dedc5930ccb9790e568f
Modupalli Jahnavi Naidu <105269496+Modupall@users.noreply.github.com>
2024-02-14 15:40:02 +0530

    Update script.js

--- cgpaand sgpa calculator/script.js
+++ cgpaand sgpa calculator/script.js
@@ -1,12 +1,4 @@
-document.getElementById("cgpaButton").addEventListener("click", function() {
-    // Logic for CGPA generation
-    document.getElementById("result").innerText = "CGPA generated!";
-  });
-  
-  document.getElementById("sgpaButton").addEventListener("click", function() {
-    // Logic for SGPA generation
-    document.getElementById("result").innerText = "SGPA generated!";
-  });
+
   document.getElementById("sgpaButton").addEventListener("click", function() {
    
     window.location.href = "sgpa.html";
